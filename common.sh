770ca17f061cb61c1301964048834e896ba71f17
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-12-07 10:32:16 +0530

    common.sh file updated

--- common.sh
+++ common.sh
@@ -2,58 +2,91 @@
 
 color="\e[35m"
 nocolor="\e[0m"
+LOGFILE=/tmp/roboshop.log
+
+check_status() {
+  if [ $1 -eq 0 ]; then
+    echo -e "${color}Status - SUCCESS${nocolor}"
+  else
+    echo -e "${color}Status - FAILED${nocolor}"
+    exit 1
+  fi
+}
+
+systemd_setup() {
+  echo -e "${color}Reload systemctl daemon${nocolor}"
+  systemctl daemon-reload &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Enable ${component} service${nocolor}"
+  systemctl enable ${component} &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Restart ${component} service${nocolor}"
+  systemctl restart ${component} &>>$LOGFILE
+  check_status $?
+}
+
+app_prereq() {
+  echo -e "${color}Copy systemd service file${nocolor}"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Add roboshop user${nocolor}"
+  id roboshop &>>$LOGFILE || useradd roboshop &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Remove existing /app directory${nocolor}"
+  rm -rf /app &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Create /app directory${nocolor}"
+  mkdir /app &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Download application content${nocolor}"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOGFILE
+  check_status $?
+
+  echo -e "${color}Extract downloaded content${nocolor}"
+  unzip /tmp/${component}.zip -d /app &>>$LOGFILE
+  check_status $?
+}
 
 nodejs_setup() {
   echo -e "${color}Disable default NodeJS${nocolor}"
-  dnf module disable nodejs -y &>>/tmp/roboshop.log
-  echo Status - $?
+  dnf module disable nodejs -y &>>$LOGFILE
+  check_status $?
 
   echo -e "${color}Enable NodeJS 20${nocolor}"
-  dnf module enable nodejs:20 -y &>>/tmp/roboshop.log
-  echo Status - $?
+  dnf module enable nodejs:20 -y &>>$LOGFILE
+  check_status $?
 
   echo -e "${color}Install NodeJS 20${nocolor}"
-  dnf install nodejs -y &>>/tmp/roboshop.log
-  echo Status - $?
+  dnf install nodejs -y &>>$LOGFILE
+  check_status $?
 
-  echo -e "${color}Add roboshop user${nocolor}"
-  useradd roboshop || echo "User already exists" &>>/tmp/roboshop.log
-  echo Status - $?
+  app_prereq
 
-  echo -e "${color}Copy systemd service file${nocolor}"
-  cp ${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
-  echo Status - $?
+  echo -e "${color}Install NodeJS Dependencies${nocolor}"
+  cd /app
+  npm install &>>$LOGFILE
+  check_status $?
 
-  echo -e "${color}Remove existing application directory${nocolor}"
-  rm -rf /app &>>/tmp/roboshop.log
-  echo Status - $?
+  systemd_setup
+}
 
-  echo -e "${color}Create /app directory${nocolor}"
-  mkdir /app &>>/tmp/roboshop.log
-  echo Status - $?
+python_setup() {
+  echo -e "${color}Install Python${nocolor}"
+  dnf install python3 gcc python3-devel -y &>>$LOGFILE
+  check_status $?
 
-  echo -e "${color}Download application content${nocolor}"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>/tmp/roboshop.log
-  echo Status - $?
+  app_prereq
 
-  echo -e "${color}Extract downloaded content${nocolor}"
-  unzip /tmp/${component}.zip -d /app &>>/tmp/roboshop.log
-  echo Status - $?
+  echo -e "${color}Install Python Dependencies${nocolor}"
+  cd /app
+  pip3 install -r requirements.txt &>>$LOGFILE
+  check_status $?
 
-  echo -e "${color}Install NodeJS dependencies${nocolor}"
-  cd /app &>>/tmp/roboshop.log
-  npm install &>>/tmp/roboshop.log &>>/tmp/roboshop.log
-  echo Status - $?
-
-  echo -e "${color}Reload systemctl daemon${nocolor}"
-  systemctl daemon-reload &>>/tmp/roboshop.log
-  echo Status - $?
-
-  echo -e "${color}Enable ${component} service${nocolor}"
-  systemctl enable ${component} &>>/tmp/roboshop.log
-  echo Status - $?
-
-  echo -e "${color}Restart ${component} service${nocolor}"
-  systemctl restart ${component} &>>/tmp/roboshop.log
-  echo Status - $?
+  systemd_setup
 }
